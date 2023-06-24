b0a9f2ca58c504d90634bc84fc2d0fea603ad4e9
Daniel Khabibullin <i6unny@gmail.com>
2023-06-24 15:51:02 +0500

    Initialize CRA

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import style from './App.module.scss';
+import {Container} from './module/Container/Container';
+
+function App() {
+	return (
+		<div className={style.app}>
+			<Container>
+			</Container>
+		</div>
+	);
+}
+
+export default App;
